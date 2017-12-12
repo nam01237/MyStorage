1273daee85bb0e1d60af7ee104660cc10c2f48cf
NamMin <name01237@naver.com>
2017-12-12 23:49:44 +0900

    dist-12

--- MyStorage_01/Client_03/MainForm.Designer.cs
+++ MyStorage_01/Client_03/MainForm.Designer.cs
@@ -325,6 +325,7 @@
             this.Controls.Add(this.list_File);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "NamHard_03";
             this.Shown += new System.EventHandler(this.Form1_Shown);
